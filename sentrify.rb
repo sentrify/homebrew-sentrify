3a92993b4d4e30a439ca8d5ab5a8da9982b0e0af
Gautam Veldanda <madhuveldanda.cse@gcet.edu.in>
2026-05-12 07:33:57 +0530

    Update sentrify.rb

--- sentrify.rb
+++ sentrify.rb
@@ -1,7 +1,7 @@
 class Sentrify < Formula
   desc "sentrify - coming soon"
-  homepage "https://github.com/YOURUSERNAME/sentrify"
-  url "https://github.com/YOURUSERNAME/sentrify/archive/refs/tags/v0.0.1.tar.gz"
+  homepage "https://github.com/sentrify/sentrify"
+  url "https://github.com/sentrify/sentrify/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   license "MIT"
   version "0.0.1"
